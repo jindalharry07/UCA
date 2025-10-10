5c150a4ee9aecf0d9cebb6c9870a1014c6aa37c5
jindalharry07 <jindalharry07@gmail.com>
2025-10-10 10:56:55 +0530

    Started nextjs with Vaibhav Sir

---
+++ VaibhavSir/nextjs/my-app/src/app/components/Description.js
@@ -0,0 +1,14 @@
+"use client";
+import { useEffect } from "react";
+
+export function DesciptionComponent(props) {
+  useEffect(() => {
+    console.log("Component Mountes");
+  }, []);
+  return (
+    // props are immutable
+    <>
+      <h1>React maneged code {props.name ?? "Guest name"}</h1>
+    </>
+  );
+}
---
+++ VaibhavSir/nextjs/my-app/src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { DesciptionComponent } from "./components/Description.js";
+export default function Home() {
+  return (
+    <div>
+      <DesciptionComponent name = "VALID NAME" ></DesciptionComponent>
+    </div>
+  );
+}
